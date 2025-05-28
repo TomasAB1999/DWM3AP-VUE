ae3e132a8849b21a1ba966fa17c757773c747971
Tomas <tomas.aabregu@davinci.edu.ar>
2025-05-28 12:37:24 -0300

    init

---
+++ src/views/ListadoView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Titulo from '../components/TituloVue.vue'
+import Template from '../components/Template.vue'
+</script>
+<template>
+  <titulo texto="Hola soy un titulo" />
+  <Template>
+    <template #parrafo>
+      <p>Esto se muestra solo en parrafo</p>
+    </template>
+    <p>Todo lo que este dentro va a slot</p>
+  </Template>
+</template>
